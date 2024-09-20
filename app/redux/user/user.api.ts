1fe6c74fd5f71d2218746e1024e9aab72d7b125c
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-09-20 04:07:41 +0100

    updated

--- app/redux/user/user.api.ts
+++ app/redux/user/user.api.ts
@@ -1,18 +1,37 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery,  } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL, // your base URL
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.accessToken;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getAllUsers: builder.query({
       query: () => "users",
     }),
+    getUserPreferences: builder.query({
+      query: () => "users/preferences",
+    }),
+    updatePreferences: builder.mutation({
+      query: (preferences) => ({
+        url: "users/preferences",
+        method: "PATCH",
+        body: preferences,
+      }),
+    }),
   }),
 });
 
-export const { useGetAllUsersQuery } = userApi;
-
-
-
+export const {
+  useGetAllUsersQuery,
+  useGetUserPreferencesQuery,
+  useUpdatePreferencesMutation,
+} = userApi;
---
+++ app/settings/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import {
+  useUpdatePreferencesMutation,
+  useGetUserPreferencesQuery,
+} from "@/app/redux/user/user.api";
+
+const Settings: React.FC = () => {
+  const { data: preferences } = useGetUserPreferencesQuery({});
+  const [notificationsEnabled, setNotificationsEnabled] = useState(
+    preferences?.notificationsEnabled || true
+  );
+  const [updatePreferences] = useUpdatePreferencesMutation();
+
+  useEffect(() => {
+    if (preferences) {
+      setNotificationsEnabled(preferences.notificationsEnabled);
+    }
+  }, [preferences]);
+
+  const handleToggle = async () => {
+    await updatePreferences({ notificationsEnabled: !notificationsEnabled });
+    setNotificationsEnabled(!notificationsEnabled);
+    notificationsEnabled
+      ? alert("Notification successfully disabled")
+      : alert("Notification successfully enabled");
+  };
+
+  return (
+    <div className="text-center flex flex-col pt-[70px] items-center min-h-screen text-black bg-gray-100 font-[family-name:var(--font-geist-sans)]">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold">Notification Settings</h2>
+        <div className="mt-4 flex  justify-around">
+          <div className="text-lg max-w-[280px]">
+            Check the box to receive Email Notifications on task updates
+          </div>
+          <input
+            type="checkbox"
+            checked={notificationsEnabled}
+            onChange={handleToggle}
+            className=" w-[30px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
