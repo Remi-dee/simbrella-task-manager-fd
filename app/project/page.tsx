8bb96087e1ce65f65923f671175d7f650d57f097
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-09-24 01:56:57 +0100

    updated

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 import ProjectList from "../components/project/ProjectList";
 import { useSelector } from "react-redux";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RootState } from "../redux/store";
 
 const ProjectsPage = () => {
@@ -11,12 +11,23 @@ const ProjectsPage = () => {
   // Get user info from redux store
   const accessToken = useSelector((state: RootState) => state.auth.accessToken);
 
-  // If the user is not authenticated, redirect to login
+  // Track when the component is mounted on the client-side
+  const [isMounted, setIsMounted] = useState(false);
+
   useEffect(() => {
+    // Set mounted flag to true once the component has mounted on the client
+    setIsMounted(true);
+
+    // If the user is authenticated, redirect to dashboard
     if (!accessToken) {
       router.push("/login");
     }
   }, [accessToken, router]);
+
+  // Prevent server-side rendering mismatch by rendering nothing during SSR
+  if (!isMounted) {
+    return null; // Prevent rendering on server-side
+  }
 
   return <div>{accessToken ? <ProjectList /> : null}</div>;
 };
--- app/task/page.tsx
+++ app/task/page.tsx
@@ -3,7 +3,7 @@
 import { useSelector } from "react-redux";
 import TaskList from "../components/task/TaskList";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RootState } from "../redux/store";
 
 const TaskPage = () => {
@@ -12,12 +12,23 @@ const TaskPage = () => {
   // Get user info from redux store
   const accessToken = useSelector((state: RootState) => state.auth.accessToken);
 
-  // If the user is not authenticated, redirect to login
+  // Track when the component is mounted on the client-side
+  const [isMounted, setIsMounted] = useState(false);
+
   useEffect(() => {
+    // Set mounted flag to true once the component has mounted on the client
+    setIsMounted(true);
+
+    // If the user is authenticated, redirect to dashboard
     if (!accessToken) {
       router.push("/login");
     }
   }, [accessToken, router]);
+
+  // Prevent server-side rendering mismatch by rendering nothing during SSR
+  if (!isMounted) {
+    return null; // Prevent rendering on server-side
+  }
 
   return (
     <div className="auth-container">{accessToken ? <TaskList /> : null}</div>
